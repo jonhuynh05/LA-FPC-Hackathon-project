5a4da05d435ad9f7b069c7f30c3acc3d3629b396
Austin <wasuen@gmail.com>
2019-09-16 15:50:05 -0700

    add signing

--- la-fpc/src/components/App/index.js
+++ la-fpc/src/components/App/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Homepage from '../Homepage'
+import SignIn from '../SignIn';
 
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
             <div>
                 <h1>Hi</h1>
                 <Homepage />
+                <SignIn />
             </div>
         )
     }
---
+++ la-fpc/src/components/SignIn/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+
+class SignIn extends Component {
+    render() {
+        return (
+            <div>
+                <h1>SignIn</h1>
+            </div>
+        )
+    }
+}
+
+export default SignIn
