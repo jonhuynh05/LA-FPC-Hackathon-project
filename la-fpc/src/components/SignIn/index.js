c50b2ffab469e0c88c87450aef540b6089b701d3
Demarkus Matthews <dmanthony@Demarkuss-MacBook-Pro.local>
2019-09-16 16:47:35 -0700

    made addAdmin

--- la-fpc/src/components/SignIn/index.js
+++ la-fpc/src/components/SignIn/index.js
@@ -3,13 +3,37 @@ import { Route, Switch } from 'react-router-dom'
 
 
 class SignIn extends Component {
-    render() {
-        return (
-            <div>
-                <h1>SignIn</h1>
-            </div>
-        )
-    }
+
+  state = {
+    name: '',
+    password: '',
+  }
+
+  onInputChange = (e) => { this.setState({ [e.target.name]: e.target.value }) 
+  };
+
+  render() {
+      return (
+          <div>
+              <h1>SignIn</h1>
+              <form onSubmit={this.submit}>
+                <div>
+                  <input type="text" placeholder="name" name="name" onChange={this.onInputChange} />
+                  <div>
+                    <small></small>
+                  </div>
+                </div>
+                <div>
+                  <input type="password" placeholder="password" name="password" onChange={this.onInputChange} />
+                  <div>
+                    <small></small>
+                  </div>
+                </div>
+                <button>SignIn</button>
+              </form>
+          </div>
+      )
+  }
 }
 
 export default SignIn
---
+++ la-fpc/src/components/addAdmin/index.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react'
+import { Route, withRouter } from 'react-router-dom'
+
+
+class addAdmin extends Component {
+
+  state = {
+    name: '',
+    password: '',
+    repassword: ''
+  }
+
+  onInputChange = (e) => { this.setState({ [e.target.name]: e.target.value }) 
+  };
+
+  validate = () => {
+    if (this.state.name.length < 1) {
+      this.setState({
+        error: {
+          name: 'please fill out name!!!'
+        }
+      })
+      return false
+    }
+    if(
+      (this.state.password !== this.state.repassword) || 
+      (this.state.password.length < 4) ||
+      (this.state.password.search(/[a-z]/) === -1) ||
+      (this.state.password.search(/[0-9]/) === -1) ||
+      (this.state.password.search(/[A-Z]/) === -1 )
+      ) {
+      this.setState({
+        error: {
+          password: 'passwords must match,length of 5 with a upper/lower case letter!!!' 
+        }
+      })
+        return false
+      }
+    return true
+  };
+
+  submit = async (e) => {
+    e.preventDefault();
+
+    const isValid = this.validate();
+    if(isValid) {
+        const registerCall = this.props.register(this.state);
+        registerCall.then((data) => {
+          console.log(data, 'this is data from register')
+          this.props.history.push('/home')
+        })
+    }
+  }
+
+  render() {
+      return (
+          <div>
+              <h1>Add Admin</h1>
+              <form onSubmit={this.submit}>
+                <div>
+                  <input type="text" placeholder="name" name="name" onChange={this.onInputChange} />
+                  <div>
+                    <small></small>
+                  </div>
+                </div>
+                <div>
+                  <input type="password" placeholder="password" name="password" onChange={this.onInputChange} />
+                  <div>
+                    <small></small>
+                  </div>
+                </div>
+                <div>
+                  <input type="password" placeholder="re-password" name="re-password" onChange={this.onInputChange} />
+                  <div>
+                    <small></small>
+                  </div>
+                </div>
+                <button>SignIn</button>
+              </form>
+          </div>
+      )
+  }
+}
+
+export default withRouter(addAdmin)
