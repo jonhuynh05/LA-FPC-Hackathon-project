25f5d5452dfe1ee5e6fd50c25a4ba2320bfe5fc4
Austin <wasuen@gmail.com>
2019-09-18 14:24:08 -0700

    add nav banner and styling

--- la-fpc/src/components/AdminButton/index.js
+++ la-fpc/src/components/AdminButton/index.js
@@ -3,13 +3,16 @@ import { Route, Switch } from 'react-router-dom'
 import { Link as LinkRoute } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import logo from "./img/FPC-Logo.jpg"
+import logo from "./img/FPC-Logo.png"
+import Banner from "./img/apples-banner.png"
+import HomeIcon from '@material-ui/icons/Home';
 
 
 class AdminButton extends Component {
 
   render() {
       return (
+        <div style={{backgroundImage:`url(${Banner})`, backgroundPosition:'center', backgroundSize:'cover'}}>
           <div style={{marginLeft: '30px', marginRight: '30px'}}>
             <Grid container spacing={3} style={{alignItems:'center'}}>
               <Grid item xs>
@@ -20,25 +23,33 @@ class AdminButton extends Component {
               <Grid item xs>
                 <div>
                   <div style={{textAlign:'center', alignItems: 'center'}}>
-                    <Button size='large' component={ LinkRoute } to='/home'>
+                    <Button size='large' component={ LinkRoute } to='/home' style={{color:'white', fontWeight: 'bold'}}>
                       LAFPC Food System Dashboard
                     </Button>
                   </div>
                 </div>
               </Grid>
               <Grid item xs style={{textAlign:"right"}}>
+                <Button
+                  component={ LinkRoute } 
+                  to='/'
+                  color="inherit"
+                  style={{margin:'0', color:'white', fontWeight:'bold'}}
+                >
+                  <HomeIcon />
+                </Button>
                 <Button 
                 component={ LinkRoute } 
                 to='/SignIn' 
                 color="inherit"
-                variant="outlined"
-                style={{margin:'20px'}}
+                style={{margin:'20px', color:'white', fontWeight:'bold'}}
                 >
                   Admin
                 </Button>
               </Grid>
             </Grid>
           </div>
+        </div>
       )
   }
 }
