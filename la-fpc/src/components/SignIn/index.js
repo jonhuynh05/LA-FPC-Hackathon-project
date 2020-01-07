abe75365b6615d293823f43ba480d9c5ec48a019
Andrew Singer <andrew.singer7@gmail.com>
2020-01-07 14:36:35 -0800

    initial commit

--- la-fpc/src/components/SignIn/index.js
+++ la-fpc/src/components/SignIn/index.js
@@ -11,22 +11,23 @@ class SignIn extends Component {
     password: '',
   }
 
-  onInputChange = (e) => { this.setState({ [e.target.name]: e.target.value }) 
+  onInputChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
   };
 
   validate = () => {
-    if(
-      (this.state.password.length < 1) || 
+    if (
+      (this.state.password.length < 1) ||
       (this.state.username.length < 1)
-      ) {
+    ) {
       this.setState({
         error: {
-          password: 'passwords must match,length of 5 with a upper/lower case letter!!!' 
+          password: 'passwords must match,length of 5 with a upper/lower case letter.'
         }
       })
-        return false
-      } else {
-        return true
+      return false
+    } else {
+      return true
     }
   };
 
@@ -34,63 +35,63 @@ class SignIn extends Component {
     console.log('hitting')
     e.preventDefault();
     const isValid = this.validate();
-    if(isValid) {
-        const login = this.props.login(this.state)
-        login.then(() => {this.props.history.push('/admin-home')})
-        console.log(this.props)
+    if (isValid) {
+      const login = this.props.login(this.state)
+      login.then(() => { this.props.history.push('/admin-home') })
+      console.log(this.props)
     }
   }
 
   render() {
-      return (
+    return (
+      <div>
+        <h1 style={{ textAlign: 'center' }}>SignIn</h1>
+        <form onSubmit={this.submit} style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
           <div>
-              <h1 style={{textAlign:'center'}}>SignIn</h1>
-              <form onSubmit={this.submit} style={{display:'flex', alignItems:'center', flexDirection:'column'}}>
-                <div>
-                  <TextField
-                    label='Username' 
-                    type="text" 
-                    placeholder="username" 
-                    name="username" 
-                    onChange={this.onInputChange}
-                    margin="normal"
-                    variant="outlined"
-                  />
-                  <div>
-                    <small></small>
-                  </div>
-                </div>
-                <div>
-                  <TextField 
-                    type="Password" 
-                    placeholder="password" 
-                    name="password" 
-                    onChange={this.onInputChange}
-                    margin="normal"
-                    variant="outlined"
-                  />
-                  <div>
-                    <small></small>
-                  </div>
-                </div>
-                <Button
-                  type="submit" 
-                  color="inherit"
-                  variant="outlined" 
-                  style={{margin:'5px'}} >
-                    SignIn
-                  </Button>
-                <Button 
-                  component={ LinkRoute } 
-                  to='/AddAdmin' 
-                  color="inherit"
-                  variant="outlined"
-                  style={{margin:'10px'}} >
-                    SignUp
-                </Button>
-              </form>
+            <TextField
+              label='Username'
+              type="text"
+              placeholder="username"
+              name="username"
+              onChange={this.onInputChange}
+              margin="normal"
+              variant="outlined"
+            />
+            <div>
+              <small></small>
+            </div>
           </div>
-      )
+          <div>
+            <TextField
+              type="Password"
+              placeholder="password"
+              name="password"
+              onChange={this.onInputChange}
+              margin="normal"
+              variant="outlined"
+            />
+            <div>
+              <small></small>
+            </div>
+          </div>
+          <Button
+            type="submit"
+            color="inherit"
+            variant="outlined"
+            style={{ margin: '5px' }} >
+            SignIn
+                  </Button>
+          <Button
+            component={LinkRoute}
+            to='/AddAdmin'
+            color="inherit"
+            variant="outlined"
+            style={{ margin: '10px' }} >
+            SignUp
+                </Button>
+        </form>
+      </div>
+    )
   }
 }
 
