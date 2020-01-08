b2a1cfd10f23896e4286ed37c35e5da47f2c5d82
Jonathan Huynh <jonhuynh05@gmail.com>
2020-01-07 16:43:03 -0800

    connect faq

---
+++ la-fpc/src/components/Faq/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {withRouter} from "react-router-dom"
+
+class Faq extends Component {
+    render(){
+        return(
+            <div>
+                this is faq
+            </div>
+        )
+    }
+}
+
+export default withRouter(Faq)
