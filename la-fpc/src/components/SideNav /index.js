f0a7c73e4e344e3153f8e394bc23ab92a5f1f782
Aaron Ryder <aaronryder@Aarons-MacBook-Pro.local>
2020-01-08 14:18:23 -0800

    sidebar v1

--- la-fpc/src/components/SideNav /index.js
+++ la-fpc/src/components/SideNav /index.js
@@ -184,6 +184,7 @@ export default function SideNav() {
                     </ListItem>
                 </List>
             </Collapse>
+
             <ListItem button onClick={handleOpenSettings}>
                 <ListItemIcon>
                     <LocalHospitalIcon /> 
@@ -191,6 +192,38 @@ export default function SideNav() {
                 <ListItemText primary="Health" />
                 {open ? <ExpandLess /> : <ExpandMore />}
             </ListItem>
+            <Collapse in={openCollapse} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                    <ListItem button className={classes.nested}>
+                        <ListItemText primary="Increased household food security"/>
+                    </ListItem>
+                    <ListItem button className={classes.nested}>
+                        <ListItemText primary="Increased healthy food access and consumption among low-income populations"/>
+                    </ListItem>
+                    <ListItem button className={classes.nested}>
+                        <ListItemText primary="Improved participation rate by eligible residents in food assistance programs"/>
+                    </ListItem>
+                    <ListItem button className={classes.nested}>
+                        <ListItemText primary="Improved participation rate by eligible students in school meal programs"/>
+                    </ListItem>
+                    <ListItem button className={classes.nested}>
+                        <ListItemText primary="Increased healthy food retailers accepting food assistance programs"/>
+                    </ListItem>
+                    <ListItem button className={classes.nested}>
+                        <ListItemText primary="Decreased racial and ethnic disparities in food security"/>
+                    </ListItem>
+                    <ListItem button className={classes.nested}>
+                        <ListItemText primary="Decreased racial and ethnic disparities in food security"/>
+                    </ListItem>
+                    <ListItem button className={classes.nested}>
+                        <ListItemText primary="Decreased generational disparities in food security and health outcomes"/>
+                    </ListItem>
+                    <ListItem button className={classes.nested}>
+                        <ListItemText primary="Reduced overweight, obesity and nutrition-related disease among low-income populations"/>
+                    </ListItem>
+                </List>
+            </Collapse>
+            
             <ListItem button onClick={handleOpenSettings}>
                 <ListItemIcon>
                     <MonetizationOnIcon/> 
@@ -218,7 +251,7 @@ export default function SideNav() {
           ))} */}
         <Divider />
         <List>
-          {['About LAFPC', 'Resources'].map((text, index) => (
+          {['FAQ', 'Resources'].map((text, index) => (
             <ListItem button key={text}>
               <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
               <ListItemText primary={text} />
