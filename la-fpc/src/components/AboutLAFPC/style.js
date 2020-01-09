778c5a1169584a58b091a318cd29537495e09115
Aaron Ryder <aaronryder@Aarons-MacBook-Pro.local>
2020-01-08 23:15:14 -0800

    adds FAQ and about components

---
+++ la-fpc/src/components/AboutLAFPC/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+export const DescribSec = styled.div`
+ text-align: center;
+ margin: 0 10% 0 10%;
+`
+export const DescribPar = styled.p`
+ text-align: left;
+ margin: 2% 0 2% 0;
+ `
